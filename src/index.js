f4859794de022348e9865c62f85c00aae6f9d88e
Linas Mickevicius <linasmnew@gmail.com>
2018-03-07 13:57:21 +0000

    added cursor blinking

--- src/index.js
+++ src/index.js
@@ -2,6 +2,67 @@ import React, { Component } from "react";
 
 let lastTime = performance.now();
 
+const myCSS = `
+@keyframes typing-effect-react {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+@-moz-keyframes typing-effect-react {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+@-webkit-keyframes typing-effect-react {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+@-ms-keyframes typing-effect-react {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+@-o-keyframes typing-effect-react {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}`;
+
 class TypeEffect extends Component {
   state = {
     output: ""
@@ -9,6 +70,17 @@ class TypeEffect extends Component {
   index = 0;
   rafRef = null;
   timeoutRef = null;
+
+  componentWillMount() {
+    const exists = document.head.querySelectorAll("[data-typing-effect-react]");
+
+    if (!exists.length) {
+      const styleElement = document.createElement("style");
+      styleElement.setAttribute("data-typing-effect-react", "true");
+      document.head.appendChild(styleElement);
+      styleElement.textContent = myCSS;
+    }
+  }
 
   componentDidMount() {
     // kick the animation off
@@ -59,7 +131,9 @@ class TypeEffect extends Component {
     }
     lastTime = time;
     // add the next character to output
-    this.setState({ output: text.substr(0, this.state.output.length + 1) });
+    this.setState({
+      output: text.substr(0, this.state.output.length + 1)
+    });
     // if more characters left to type out,
     // then register a call to yourself again to take care of the next character, and so on...
     if (this.state.output.length < text.length) {
@@ -100,9 +174,18 @@ class TypeEffect extends Component {
   };
 
   render() {
+    const blinkingStyles = {
+      WebkitAnimation: "1s typing-effect-react step-end infinite",
+      MozAnimation: "1s typing-effect-react step-end infinite",
+      MsAnimation: "1s typing-effect-react step-end infinite",
+      OAnimation: "1s typing-effect-react step-end infinite",
+      animation: "1s typing-effect-react step-end infinite"
+    };
+
     return (
       <span className={this.props.className} style={this.props.style}>
         {this.state.output}
+        <span style={blinkingStyles}>|</span>
       </span>
     );
   }
